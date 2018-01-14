2eddb0d26729fd8afe1cc0beb92c433a0994a9b4
Savannahbharti <34062697+Savannahbharti@users.noreply.github.com>
2018-01-15 00:06:01 +0100

    Interacties toegevoegd

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true * /
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+var section = document.getElementById('filter');
+var button = document.getElementById("sortfilterbutton");
+var show = function () {
+    section.classList.toggle('sortfilteractive');
+};
+button.addEventListener('click', show);
+section.setVisibility(button.INVISIBLE);
